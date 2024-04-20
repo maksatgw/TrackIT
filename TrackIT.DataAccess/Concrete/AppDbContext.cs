f2fcfd2eae504ed1c52524856624ff2f44516619
maksatgw <garyagdyyev@outlook.com>
2024-04-20 22:34:18 +0300

    Add project files.

---
+++ TrackIT.DataAccess/Concrete/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackIT.Business.Model;
+
+namespace TrackIT.DataAccess.Concrete
+{
+    public class AppDbContext : IdentityDbContext<AppUser, AppRole, string> //<- id için guid kullanacağız.
+    {
+        //Hangi database provider kullanacağımızı karar vermek için (program.cs) bir options parametresi göndereceğiz.
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) //<- base ile miras aldığımız sınıfın constructoruna parametre gönderiyoruz.
+        {
+
+        }
+
+    }
+}
